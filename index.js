f58aef717e738781882bd65f1448f93fb87fd73f
DanielWilliams007 <hifbs0ibdufzc@hotmail.com>
2025-04-14 09:33:18 +0800

    cli implementation

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 #!/usr/bin/env node
 
+const BlogGenerator = require('./src/generator');
+
+const args = process.argv.slice(2);
+const command = args[0] || 'build';
+
 console.log('Markdown Blog Generator v0.1.0');
-console.log('Starting...');
+
+const generator = new BlogGenerator();
+
+switch(command) {
+  case 'build':
+    generator.generate();
+    break;
+  case 'init':
+    console.log('Initializing new blog...');
+    break;
+  case 'serve':
+    console.log('Starting dev server...');
+    break;
+  default:
+    console.log(`Unknown command: ${command}`);
+    process.exit(1);
+}
